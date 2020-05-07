be5d0864547b7b28cca0a0d0bc264d92bc72e3cd
JohnCut <52240938+JohnCut@users.noreply.github.com>
2020-05-07 12:59:27 +0300

    Input object lists added.

--- lib/budgetplan.dart
+++ lib/budgetplan.dart
@@ -1,7 +1,24 @@
 import 'dart:core';
 import 'package:flutter/material.dart';
 import 'homepage.dart';
-import 'newplan.dart';
+
+class Gelir {
+  String title, unit;
+  Gelir(this.title, this.unit);
+  @override
+  String toString() {
+    return '{ ${this.title}, ${this.unit} }';
+  }
+}
+
+class Gider {
+  String title, type, unit;
+  Gider(this.title, this.type, this.unit);
+  @override
+  String toString() {
+    return '{ ${this.title}, ${this.type}, ${this.unit} }';
+  }
+}
 
 class Plan extends StatefulWidget {
   final String ihText, isText, tasText;
@@ -19,6 +36,8 @@ class _PlanState extends State<Plan> {
 
   final gelirTC = TextEditingController();
   final giderTC = TextEditingController();
+  final gelirTTC = TextEditingController();
+  final giderTTC = TextEditingController();
   String ihKB = ''; // ih kalan bütçe
   String isKB = ''; // is kalan bütçe
   String anTas = ''; // anlık tasarruf
@@ -30,6 +49,8 @@ class _PlanState extends State<Plan> {
   String giderSum = ''; // gider toplamı (gider list elemanları toplamı)
   String ihSum = ''; // ih gider toplamı (ih gider list elemanları toplamı)
   String isSum = ''; // is gider toplamı (is gider list elemanları toplamı)
+  List gelirler = [];
+  List giderler = [];
   // 4 TANE LIST OLUŞTURULACAK. (GELİR, GİDER, İH GİDER, İS GİDER)
   // GİDERLER HEM KENDİ KATEGORİSİNİN LIST İNE HEM DE GİDER LIST İNE KAYDEDİLECEK.
 
@@ -49,8 +70,11 @@ class _PlanState extends State<Plan> {
           IconButton(
               icon: Icon(Icons.home),
               onPressed: () {
-                Navigator.push(context,
-                      MaterialPageRoute(builder: (context) => Homepage(ihText : ihText, isText : isText, tasText : tasText)));
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => Homepage(
+                            ihText: ihText, isText: isText, tasText: tasText)));
               }),
           IconButton(
               icon: Icon(Icons.view_list),
@@ -61,8 +85,7 @@ class _PlanState extends State<Plan> {
         ],
       ),
       body: Center(
-        child: Column(
-        children: [
+        child: Column(children: [
           Container(
             margin: const EdgeInsets.all(15.0),
             padding: const EdgeInsets.all(10.0),
@@ -135,8 +158,8 @@ class _PlanState extends State<Plan> {
             ]),
           ),
           Container(
-            // margin: const EdgeInsets.all(15.0),
-            // decoration: BoxDecoration(border: Border.all()),
+              // margin: const EdgeInsets.all(15.0),
+              // decoration: BoxDecoration(border: Border.all()),
               child: Expanded(
             child: Row(
               children: <Widget>[
@@ -146,15 +169,48 @@ class _PlanState extends State<Plan> {
                       Text('Gelir giriniz.'),
                       Flexible(
                           child: TextField(
+                        controller: gelirTTC,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Gelir İsmi'),
+                      )),
+                      Flexible(
+                          child: TextField(
                         controller: gelirTC,
-                        decoration:
-                            InputDecoration(border: OutlineInputBorder()),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Gelir Değeri'),
                         keyboardType: TextInputType.number,
                       )),
-                      RaisedButton(onPressed: () {
-                        // value yu listeye kaydetme işlemi
-                      },
-                      child: Text('Geliri kaydet'),
+                      RaisedButton(
+                        onPressed: () {
+                          if (gelirTTC.text == '' || gelirTC.text == '') {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    content: Text(
+                                        'LÜTFEN GEREKLİ ALANLARI DOLDURUN.'),
+                                    contentPadding: const EdgeInsets.all(16.0),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                          child: Text('TAMAM'),
+                                          textColor: Color(0xFFB6B6B6),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          }),
+                                    ],
+                                  );
+                                });
+                          } else if (gelirTTC.text != '' &&
+                              gelirTC.text != '') {
+                            gelirAdd(
+                                gelirTTC.text, gelirTC.text); // ekleme işlemi
+                            print('GELİRLER LİST LENGTH: ${gelirler.length}');
+                            print('GELİRLER LİST: $gelirler');
+                          }
+                        },
+                        child: Text('Geliri kaydet'),
                       ),
                     ],
                   ),
@@ -181,15 +237,48 @@ class _PlanState extends State<Plan> {
                       ),
                       Flexible(
                           child: TextField(
+                        controller: giderTTC,
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Gelir İsmi'),
+                      )),
+                      Flexible(
+                          child: TextField(
                         controller: giderTC,
-                        decoration:
-                            InputDecoration(border: OutlineInputBorder()),
+                        decoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            labelText: 'Gider Değeri'),
                         keyboardType: TextInputType.number,
                       )),
-                      RaisedButton(onPressed: () {
-                        // value yu hem giderin kendi kategorisinin list ine hem e gider list ine kaydet
-                      },
-                      child: Text('Gideri kaydet'),
+                      RaisedButton(
+                        onPressed: () {
+                          if (giderTTC.text == '' || giderTC.text == '') {
+                            showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return AlertDialog(
+                                    content: Text(
+                                        'LÜTFEN GEREKLİ ALANLARI DOLDURUN.'),
+                                    contentPadding: const EdgeInsets.all(16.0),
+                                    actions: <Widget>[
+                                      FlatButton(
+                                          child: Text('TAMAM'),
+                                          textColor: Color(0xFFB6B6B6),
+                                          onPressed: () {
+                                            Navigator.pop(context);
+                                          }),
+                                    ],
+                                  );
+                                });
+                          } else if (giderTTC.text != '' &&
+                              giderTC.text != '') {
+                            giderAdd(giderTTC.text, gidDBValue,
+                                giderTC.text); // ekleme işlemi
+                            print('GİDERLER LİST LENGTH: ${giderler.length}');
+                            print('GİDERLER LİST: $giderler');
+                          }
+                        },
+                        child: Text('Gideri kaydet'),
                       ),
                     ],
                   ),
@@ -198,25 +287,41 @@ class _PlanState extends State<Plan> {
             ),
           )),
           Container(
-            margin: const EdgeInsets.all(15.0),
-            padding: const EdgeInsets.all(5.0),
-            decoration: BoxDecoration(border: Border.all()),
+              margin: const EdgeInsets.all(15.0),
+              padding: const EdgeInsets.all(5.0),
+              decoration: BoxDecoration(border: Border.all()),
               child: Row(
-            children: <Widget>[
-              Text('Gelir Toplamı: $gelirSum'), // gelir list elemanları toplamı
-              Expanded(
-                child: Column(
-                  children: <Widget>[
-                    Text('İH toplamı: $ihSum'), // ih gider list elemanları toplamı
-                    Text('İS Toplamı: $isSum'), // is gider list elemanları toplamı
-                    Text('Gider Toplamı: $giderSum'), // gider list elemanları toplamı
-                  ],
-                ),
-              )
-            ],
-          ))
+                children: <Widget>[
+                  Text(
+                      'Gelir Toplamı: $gelirSum'), // gelir list elemanları toplamı
+                  Expanded(
+                    child: Column(
+                      children: <Widget>[
+                        Text(
+                            'İH toplamı: $ihSum'), // ih gider list elemanları toplamı
+                        Text(
+                            'İS Toplamı: $isSum'), // is gider list elemanları toplamı
+                        Text(
+                            'Gider Toplamı: $giderSum'), // gider list elemanları toplamı
+                      ],
+                    ),
+                  )
+                ],
+              ))
         ]),
       ),
     ));
   }
+
+  gelirAdd(String title, unit) {
+    setState(() {
+      gelirler.add((Gelir(title, unit)));
+    });
+  } // değerleri gelirler list ine ekler.
+
+  giderAdd(String title, type, unit) {
+    setState(() {
+      giderler.add((Gider(title, type, unit)));
+    });
+  } // değerleri giderler list ine ekler.
 }
